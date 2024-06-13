be2c09395a4676847affb571b3b6c70ac1094fcc
Rory Magee <roryemagee@gmail.com>
2024-06-13 13:04:12 -0600

    Setup basic Express backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("API is running...");
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
