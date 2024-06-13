1197d794343d1d9514624390d8a6ad7d9e857791
Rory Magee <roryemagee@gmail.com>
2024-06-13 13:47:27 -0600

    Add Nodemon package and Refactor server.js to use .env variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import products from './data/products.js' 
+import dotenv from 'dotenv';
+dotenv.config();
+import products from './data/products.js';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
