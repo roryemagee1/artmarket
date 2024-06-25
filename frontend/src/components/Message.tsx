02d124ffd36fed2c4bbaf3a835076c5de3d47521
Rory Magee <roryemagee@gmail.com>
2024-06-25 14:37:26 -0700

    Add temporary parseWorkaround function

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -6,10 +6,24 @@ interface IMessage {
   children: string;
 }
 
+// The parseWorkaround should be removed in the final production version of this application.  I am only including it now because there is an issue with the data key being unknown type.
+
 export default function Message({ variant="info", children }: IMessage): JSX.Element {
+  let evalBool: boolean = false;
+  let output = "";
+  if (typeof children === "object") {
+    evalBool = true
+    output = parseWorkaround();
+    console.log(output);
+  }
+
+  function parseWorkaround() {
+    return eval("children?.data?.message");
+  }
+
   return (
     <Alert variant={variant}>
-      {children}
+      {evalBool ? output : children}
     </Alert>
   )
 }
