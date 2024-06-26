ca1f492dba757fd185af805f29817267922a0d7c
Rory Magee <roryemagee@gmail.com>
2024-06-26 15:09:56 -0700

    Add current item indicator Badge to Header component and Add state and dispatch types to store.ts

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import { JSX } from 'react'
 import { FaShoppingCart, FaUser} from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux'
+
+import type { RootState } from '@src/store'
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import logo from '@src/assets/audasite.png'
+import Badge from 'react-bootstrap/Badge'
 
 export default function Header(): JSX.Element {
+  const  { cartItems }  = useSelector((state: RootState) => state.cart)
+  console.log(cartItems);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -24,6 +31,13 @@ export default function Header(): JSX.Element {
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
+                  {
+                    cartItems.length > 0 && (
+                      <Badge pill bg="success" style={{marginLeft: "5px"}}>
+                        {cartItems.reduce((acc: number, curr: { price: number, qty: number }) => acc + curr.qty, 0)}
+                      </Badge>
+                    )
+                  }
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
