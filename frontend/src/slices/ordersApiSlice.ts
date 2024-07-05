e6edc083983e9b9de31845ee1212fa5847181de8
Rory Magee <roryemagee@gmail.com>
2024-07-04 21:38:39 -0700

    Create ordersApiSlice associated functionality

---
+++ frontend/src/slices/ordersApiSlice.ts
@@ -0,0 +1,28 @@
+import { ORDERS_URL } from '@src/constants';
+import { apiSlice } from '@src/slices/apiSlice'
+
+export const ordersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (order) => ({
+        url: `${ORDERS_URL}`,
+        method: "POST",
+        body: { ...order },
+      })
+    }),
+    getMyOrders: builder.query({
+      query: () => ({
+        url: `${ORDERS_URL}/myorders`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getOrderById: builder.query({
+      query: (id) => ({
+        url: `${ORDERS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useCreateOrderMutation, useGetMyOrdersQuery, useGetOrderByIdQuery } = ordersApiSlice;
