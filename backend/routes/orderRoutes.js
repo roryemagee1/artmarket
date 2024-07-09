edb079599883e9c31286f5fc5659721bb76c5b92
Rory Magee <roryemagee@gmail.com>
2024-07-08 19:28:25 -0600

    Remove requirement of admin privileges for using getOrderById

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -19,7 +19,7 @@ router.route('/myorders')
   .get(getMyOrders)
 
 router.route('/:id')
-  .get(protect, admin, getOrderById);
+  .get(protect, getOrderById);
 
 router.route('/:id/pay')
   .put(protect, updateOrderToPaid)
