bd060d83c1bc467fd2efe9cbeed0bd8c542f6d0a
Rory Magee <roryemagee@gmail.com>
2024-07-09 11:55:56 -0600

    Add functionality to updateOrderToPaid and updateOrderToDelivered functions

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -102,14 +102,48 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @routes PUT /api/orders/:id/pay
 // @access Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("Update order to paid.");
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_adress: req.body.email_address,
+    }
+    const updatedOrder = await order.save();
+
+    res.status(201).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found.");
+  }
 });
 
 // @desc Update order to delivered
 // @routes PUT /api/orders/:id/deliver
 // @access Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send("Update order to delivered.");
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+    
+    const updatedOrder = await order.save();
+
+    res.status(201).json(updatedOrder);
+  } else {
+    order.isDelivered = true;
+    const updatedOrder = await order.save();
+
+    res.status(201).json(updatedOrder);
+
+    res.status(404);
+    throw new Error("Order not found.");
+  }
 });
 
 // @desc Get all orders
