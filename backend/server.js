80a2971e89c8e7982afe77cebb316b4a21304ba6
Rory Magee <roryemagee@gmail.com>
2024-07-11 15:37:18 -0600

    Implement multer on the backend and Create uploadRoutes.js for uploading images to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 connectDB();
 const app = express();
@@ -20,13 +22,17 @@ app.use(cors(corsOptions), express.json(), express.urlencoded({ extended: true }
 
 app.get('/', (req, res) => {
   res.send("API is running...");
-})
+});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound, errorHandler);
 
