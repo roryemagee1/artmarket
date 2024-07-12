e6ff3cc54fe3e9f98d966a079d572a2cca2ffb66
Rory Magee <roryemagee@gmail.com>
2024-07-11 19:38:42 -0600

    Add /uploads proxy to vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
-      }
+      },
+      "/uploads": 'http://localhost:3000',
     }
   },
   resolve: {
