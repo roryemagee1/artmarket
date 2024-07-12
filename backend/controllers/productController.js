fb579370ebf057763e4dc3c58c103e58970935a2
Rory Magee <roryemagee@gmail.com>
2024-07-12 14:05:11 -0600

    Complete deleteProduct function in productController.js and add deleteProduct route in productRoutes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -114,11 +114,29 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Delete a product
+// @routes DELETE /api/products/:id
+// @access Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Product deleted!",
+    });
+  } else {
+    res.status(404);
+    throw new Error("Product failed to be deleted.");
+  }
+})
+
 export { 
   getProducts, 
   getProductById, 
   createProduct, 
-  updateProduct 
+  updateProduct,
+  deleteProduct,
 }
 
 // const productSchema = new mongoose.Schema({
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { getProducts, getProductById, createProduct, updateProduct } from '../controllers/productController.js';
+import { 
+  getProducts, 
+  getProductById, 
+  createProduct, 
+  updateProduct, 
+  deleteProduct 
+} from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -10,6 +16,7 @@ router.route('/')
 
 router.route('/:id')
   .get(getProductById)
-  .put(protect, admin, updateProduct);
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
