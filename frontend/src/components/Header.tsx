bff6041f62deeb57acecb6647bebe5e0eee9e2d1
Rory Magee <roryemagee@gmail.com>
2024-07-17 15:20:23 -0600

    Refactor Header component to remove LinkContainer component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { JSX } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { FaShoppingCart, FaUser} from 'react-icons/fa'
-import { LinkContainer } from 'react-router-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
 
@@ -55,59 +54,43 @@ export default function Header(): JSX.Element {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              <img src={logo} height="70vw" alt="Audasite LLC Logo"/>
-              ArtMarket
-            </Navbar.Brand>
-          </LinkContainer>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} height="70vw" alt="Audasite LLC Logo"/>
+            ArtMarket
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <SearchBox />
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <FaShoppingCart /> Cart
-                  {
-                    (cartItems.length > 0) && (
-                      <Badge pill bg="success" style={{marginLeft: "5px"}}>
-                        {cartItems?.reduce((acc: number, curr: { qty: number }) => acc + curr.qty, 0)}
-                      </Badge>
-                    )
-                  }
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link as={Link} to="/cart">
+                <FaShoppingCart /> Cart
+                {
+                  (cartItems.length > 0) && (
+                    <Badge pill bg="success" style={{marginLeft: "5px"}}>
+                      {cartItems?.reduce((acc: number, curr: { qty: number }) => acc + curr.qty, 0)}
+                    </Badge>
+                  )
+                }
+              </Nav.Link>
               {
                 userInfo && userInfo.isAdmin ? (
                   <NavDropdown title={userInfo.name} id="username" style={{marginLeft: "10px", marginRight: "55px"}}>
-                    <LinkContainer to="/admin/productlist">
-                      <NavDropdown.Item>Products</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/admin/userlist">
-                      <NavDropdown.Item>Users</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/admin/orderlist">
-                      <NavDropdown.Item>Orders</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </LinkContainer>
+                    <NavDropdown.Item as={Link} to="/admin/productlist">Products</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admin/userlist">Users</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admin/orderlist">Orders</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
                     <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
                   </NavDropdown>
                 ) :
                 userInfo ? (
                   <NavDropdown title={userInfo.name} id="username">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </LinkContainer>
+                    <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
                     <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
                   </NavDropdown>
                 ) : (
-                  <LinkContainer to="/login">
-                    <Nav.Link>
-                      <FaUser /> Sign In
-                    </Nav.Link>
-                  </LinkContainer>
+                  <Nav.Link as={Link} to="/login">
+                    <FaUser /> Sign In
+                  </Nav.Link>
                 )
               }
             </Nav>
