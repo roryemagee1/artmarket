0ba995c5f88c66ce92a5d0c9fbbb69757206ddd1
Rory Magee <roryemagee@gmail.com>
2024-07-18 14:17:01 -0600

    Remove Loader and isLoading from ProductCarousel component

--- frontend/src/components/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 import Carousel from 'react-bootstrap/Carousel'
 import Image from 'react-bootstrap/Image'
 
-import Loader from '@src/components/Loader'
 import Message from '@src/components/Message'
 
 import { useGetTopProductsQuery } from '@src/slices/productsApiSlice'
@@ -12,17 +11,16 @@ import { useGetTopProductsQuery } from '@src/slices/productsApiSlice'
 import { IProductKeys } from '@src/types/interfaces'
 
 export default function ProductCarousel(): JSX.Element {
-  const { data: products, isLoading, error } = useGetTopProductsQuery(null);
+  const { data: products, error } = useGetTopProductsQuery(null);
   
   return (
     <>
       {
-        isLoading ? <Loader /> :
         error ? <Message variant="danger" evalBool={false}>{`${error}`}</Message> :
         (
           <Carousel pause="hover" className="bg-primary mb-4">
             {
-              products.map((product: IProductKeys) => (
+              products?.map((product: IProductKeys) => (
                 <Carousel.Item key={product._id}>
                   <Link to={`/product/${product._id}`}>
                     <Image src={product.image} alt={product.name} fluid />
