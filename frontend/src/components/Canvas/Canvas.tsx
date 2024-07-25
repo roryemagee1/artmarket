da9c6f29e6641130c59a53c1b4495858fa4168b4
Rory Magee <roryemagee@gmail.com>
2024-07-25 12:16:03 -0600

    Refactor Canvas component props

--- frontend/src/components/Canvas/Canvas.tsx
+++ frontend/src/components/Canvas/Canvas.tsx
@@ -2,14 +2,12 @@ import { JSX } from 'react'
 import './Canvas.css'
 
 interface ICanvas {
-  height: string;
-  width: string;
+  height?: string;
+  width?: string;
   children: JSX.Element;
 }
 
-export default function Canvas({ children, height="", width=""}: ICanvas): JSX.Element {
-  console.log(height, width);
-
+export default function Canvas({ children }: ICanvas): JSX.Element {
   return (
     <div className="canvas">
       { children }
