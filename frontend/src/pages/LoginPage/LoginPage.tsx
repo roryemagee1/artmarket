051a5ad359b3334c07228610af3e65d0cf6dcac3
Rory Magee <roryemagee@gmail.com>
2024-07-24 19:34:47 -0600

    Complete refactor of LoginPage component

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -4,16 +4,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import './LoginPage.css'
 
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
 import { useLoginMutation } from '@src/slices/usersApiSlice';
 import { setCredentials } from '@src/slices/authSlice';
 
 import Loader from '@src/components/Loader'
-import FormContainer from '@src/components/FormContainer'
 
 import Background from '@src/components/Background/Background'
 
@@ -67,51 +61,47 @@ export default function LoginPage(): JSX.Element {
     <>
       <Background variant="museum" whiteBackground={false} />
       <section className="login-styling">
-        {/* <FormContainer>
-          <> */}
-            <h1>Create Account</h1>
-            <form>
-              <div>
-                <label htmlFor={id + "-email"}>Email Address</label>
-                <input
-                  name="email"
-                  id={id + "-email"}
-                  type="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                >
-                </input>
-              </div>
-              <div>
-                <label htmlFor={id + "-password"}>Password</label>
-                <input
-                  name="password"
-                  id={id + "-password"}
-                  type="password"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                >
-                </input>
-              </div>
-              <button 
-                type="submit" 
-                className="login-button"
-                onClick={(event) => handleSubmit(event)}
-                disabled={ isLoading }
-              >
-                Sign In
-              </button>
-            </form>
-            <div>
-              <p>
-                New Customer? <Link to={ redirect ? `/register?redirect=${redirect}` : `/register`}>Register</Link>
-              </p>
-            </div>
-            { isLoading && <Loader />}
-          {/* </>
-        </FormContainer> */}
+        <h1>Create Account</h1>
+        <form>
+          <div>
+            <label htmlFor={id + "-email"}>Email Address</label>
+            <input
+              name="email"
+              id={id + "-email"}
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            >
+            </input>
+          </div>
+          <div>
+            <label htmlFor={id + "-password"}>Password</label>
+            <input
+              name="password"
+              id={id + "-password"}
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            >
+            </input>
+          </div>
+          <button 
+            type="submit" 
+            className="login-button"
+            onClick={(event) => handleSubmit(event)}
+            disabled={ isLoading }
+          >
+            Sign In
+          </button>
+        </form>
+        <div>
+          <p>
+            New Customer? <Link to={ redirect ? `/register?redirect=${redirect}` : `/register`}>Register</Link>
+          </p>
+        </div>
+        { isLoading && <Loader />}
       </section>
     </>
   )
