fbf21159bfc178414448ba3165fc691adb27d065
Rory Magee <roryemagee@gmail.com>
2024-08-13 15:39:21 -0600

    Remove unnecessary code from App.css and Add purchase disclaimer to OrderPage component

--- frontend/src/pages/OrderPage/OrderPage.jsx
+++ frontend/src/pages/OrderPage/OrderPage.jsx
@@ -211,6 +211,10 @@ export default function OrderPage() {
               </section>
             </div>
 
+            <div style={{ margin: "10px 0 0 0"}}>
+              <Message variant="danger" ><strong>Disclaimer:</strong> The PayPal buttons are for testing purposes only.  Submission of a purchase record in the system does not authorize a real purchase nor does it create an obligation to deliver any goods or sevices.</Message>
+            </div>
+
             <section className="order-items">
               <ol>
                 <h2>Order Items</h2>
